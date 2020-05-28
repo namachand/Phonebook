0b73883186420224375bbafd7f85b3b659345cae
namachand <caman3874@gmail.com>
2020-05-29 03:58:08 +0530

    phonebook home page

--- client/src/components/phonebookHome.js
+++ client/src/components/phonebookHome.js
@@ -9,8 +9,7 @@ class Home extends React.Component{
         this.state={
             pager:{},
             allContacts:[]
-            }
-
+        }
     }
 
     componentDidMount(){
@@ -20,7 +19,7 @@ class Home extends React.Component{
     componentDidUpdate(){
         this.loadContactDetails()
     }
-
+    //paginating the lists of contacts 
     loadContactDetails(){
         //
         const param=new URLSearchParams(this.props.location.search);
@@ -38,66 +37,75 @@ class Home extends React.Component{
         console.log(this.state.allContacts)
         return(
         <React.Fragment>
-            <div>
-                <Link to='/createContact'><span className='fa fa-plus fa-4x' id='createContact' onClick={this.createContact}>
-                    </span></Link>
+            <div className='row'>
+                <div className='col-md-9 offset-1'>
+                    {this.state.allContacts.map((contact)=>{
+                    return(
+                        <div className='row mt-3 mb-3 storyCard' style={{display:'flex',justifyContent:'center'}}>
+                            <Card style={{
+                                minHeight:'30vh',
+                                width:'40vw',
+                                marginTop:'2vh',
+                                backgroundColor:'yellowgreen',
+                                border:'solid 1px,grey'
+                                }}>
+                                <CardBody>
+                                    <div className='row'>
+                                        <div className='col-md-12'>
+                                            <p className='contactdetail'>{contact.name}</p>
+                                            </div>                          
+                                        </div>
+                                    <div className='row'> 
+                                        <div className='col-md-3'> 
+                                            <p >{contact.dateOfBirth}</p>
+                                            </div>
+                                        <div className='col-md-3 offset-3'>
+                                            <Link to={{pathname:'/editContact',data:contact._id}}><Button className='contactdetail' id='submitDetails' style={{width:'8vw',backgroundColor:'cadetblue'}}>Edit</Button></Link>
+                                            </div>
+                                        <div className='col-md-3'>
+                                            <Button className='contactdetail' id='submitDetails' style={{width:'8vw',backgroundColor:'red'}}>Remove</Button>
+                                            </div>
+                                        </div>
+                                        <div className='row' style={{backgroundColor:'papayawhip',marginTop:'15px',minHeight:'12vh'}}>
+                                            <div className='col-md-4'>
+                                                {contact.mobileNumber.map((number)=>{
+                                                    return(
+                                                        <p className='fa fa-phone fa-lg contactdetail mt-2'>  {number}</p>
+                                                    )
+                                                })}
+                                                </div>
+                                            <div className='col-md-6 offset-2' >
+                                                {contact.email.map((mail)=>{
+                                                    return(
+                                                        <p className='fa fa-envelope fa-lg contactdetail mt-2' style={{paddingTop:'5px'}}>  {mail}</p>
+
+                                                    )
+                                                })
+                                                }
+                                                </div>
+                                            </div>
+                                    </CardBody>
+                                </Card>
+                            </div>
+                        )
+                    })}  
+                <div className="pb-0 pt-3 pageList">
+                    {this.state.pager.pages && this.state.pager.pages.length &&
+                    <ul className="pagination" style={{display:'flex',justifyContent:'center'}}>
+                        {this.state.pager.pages.map(page =>
+                        <li key={page} className={`page-item number-item ${this.state.pager.currentPage === page ? 'active' : ''}`}>
+                            <Link to={{ search: `?page=${page}` }} className="page-link">{page}</Link>
+                            </li>
+                        )}
+                        </ul>
+                    }                    
+                    </div>    
                 </div>
-            <div>
-                {this.state.allContacts.map((contact)=>{
-                return(
-                    <div className='row mt-3 mb-3 storyCard' style={{display:'flex',justifyContent:'center'}}>
-                        <Card style={{
-                            minHeight:'30vh',
-                            width:'40vw',
-                            backgroundColor:'yellowgreen',
-                            border:'solid 1px,grey'
-                            }}>
-                            <CardBody>
-                                <div className='row'>
-                                    <div className='col-md-12'>
-                                        <p className='contactdetail'>{contact.name}</p>
-                                        </div>                          
-                                    </div>
-                                <div className='row'> 
-                                    <div className='col-md-3'> 
-                                        <p >{contact.dateOfBirth}</p>
-                                        </div>
-                                    <div className='col-md-3 offset-3'>
-                                        <Button className='contactdetail' id='submitDetails' style={{width:'8vw',backgroundColor:'cadetblue'}}>Edit</Button>
-                                        </div>
-                                    <div className='col-md-3'>
-                                        <Button className='contactdetail' id='submitDetails' style={{width:'8vw',backgroundColor:'red'}}>Remove</Button>
-                                        </div>
-                                    </div>
-                                    <div className='row' style={{backgroundColor:'papayawhip',marginTop:'15px',minHeight:'12vh'}}>
-                                        <div className='col-md-4'>
-                                            {contact.mobileNumber.map((number)=>{
-                                                return(
-                                                    <p className='fa fa-phone fa-lg contactdetail mt-2'>  {number}</p>
-                                                )
-                                            })}
-                                            </div>
-                                        <div className='col-md-6 offset-2' >
-                                            <p className='fa fa-envelope fa-lg contactdetail mt-1' style={{paddingTop:'5px'}}>  {contact.email}</p>
-                                            </div>
-                                        </div>
-                                </CardBody>
-                            </Card>
-                        </div>
-                    )
-                })}    
-                </div> 
-            <div className="pb-0 pt-3 pageList">
-                {this.state.pager.pages && this.state.pager.pages.length &&
-                <ul className="pagination" style={{display:'flex',justifyContent:'center'}}>
-                    {this.state.pager.pages.map(page =>
-                    <li key={page} className={`page-item number-item ${this.state.pager.currentPage === page ? 'active' : ''}`}>
-                        <Link to={{ search: `?page=${page}` }} className="page-link">{page}</Link>
-                        </li>
-                    )}
-                    </ul>
-                }                    
-                </div>    
+                <div className='col-md-2'>
+                    <Link to='/createContact'><span className='fa fa-plus fa-4x' id='createContact' onClick={this.createContact}>
+                        </span></Link>
+                    </div> 
+            </div>
             </React.Fragment>
         )
     }
